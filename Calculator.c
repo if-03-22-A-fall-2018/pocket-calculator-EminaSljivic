993079ebee44df8e407314f29187fb4be6b423aa
Emina Sljivic <eminasljivic@Eminas-Air.mshome.net>
2018-10-09 11:05:25 +0200

    wip

--- Calculator.c
+++ Calculator.c
@@ -1,18 +1,109 @@
 #include <stdio.h>
+#include <float.h>
 
-int print_menu_and_get_operation(){
+int print_menu_and_get_operation()
+{
   int operation;
 
-do {
+  do {
 
-  printf("Choose one of the following operations: \n   Add (1) \n   Subtract (2) \n   Multiply (3) \n   Divide (4 ) \n   Stop program (-1) \nEnter your choice: ");
-  scanf("%d",&operation );
-} while(operation != 1 || operation != 2 || operation != 3|| operation !=4|| operation !=-1);
+    printf("Choose one of the following operations: \n   Add (1) \n   Subtract (2) \n   Multiply (3) \n   Divide (4) \n   Stop program (-1) \nEnter your choice: ");
+    scanf("%d",&operation );
+    if(operation != 1 && operation != 2 && operation != 3 && operation !=4 && operation !=-1){
+      printf("Input not allowed, please try again!\n");
+    }
+  } while(operation != 1 && operation != 2 && operation != 3 && operation !=4 && operation !=-1);
 
-return operation;
+  return operation;
 }
 
-int main(int argc, char const *argv[]) {
-  print_menu_and_get_operation();
+void get_opernads(double* opernad_one, double* opernad_two)
+{
+  do {
+    printf("Please enter the first operand: ");
+    scanf("%lf",opernad_one);
+    if(*opernad_one > DBL_MAX)
+    {
+      printf("Number overflow!\n");
+    }
+    else if(*opernad_one < DBL_MIN)
+    {
+      printf("Number underflow!\n");
+    }
+  } while(*opernad_one > DBL_MAX || *opernad_one < DBL_MIN);
+
+  do {
+    printf("Please enter the second operand: ");
+    scanf("%lf",opernad_two);
+    if(*opernad_two > DBL_MAX)
+    {
+      printf("Number overflow!\n");
+    }
+    else if(*opernad_two < DBL_MIN)
+    {
+      printf("Number underflow!\n");
+    }
+  } while(*opernad_two > DBL_MAX || *opernad_two < DBL_MIN);
+
+}
+
+double perform_operation(int operation,double num1, double num2)
+{
+  double result=0;
+
+  if(operation==1)
+  {
+      result=num1+num2;
+  }
+
+  else if(operation==2)
+  {
+    result=num1-num2;
+
+  }
+
+  else if(operation==3)
+  {
+
+    result=num1*num2;
+
+  }
+
+  else if(num2!=0)
+  {
+
+      result=num1/num2;
+
+  }
+
+  else
+  {
+    printf("Division by zero!\n");
+  }
+
+  return result;
+}
+
+int main(int argc, char const *argv[])
+{
+  int operation=0;
+    do {
+      double opernad_one;
+      double opernad_two;
+      double result;
+
+      operation=print_menu_and_get_operation();
+
+      if(operation!=-1)
+      {
+        get_opernads(&opernad_one, &opernad_two);
+        result=perform_operation(operation,opernad_one,opernad_two);
+
+        if(opernad_two!=0)
+        {
+          printf("The result is: %lf\n",result);
+        }
+      }
+  } while(operation!=-1);
   return 0;
 }
