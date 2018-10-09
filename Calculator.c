9d73027fa115456a749ea0e702cf16bfa4d0a97a
Emina Sljivic <emisljivic@hotmail.com>
2018-10-09 22:42:00 +0200

    finished

--- Calculator.c
+++ Calculator.c
@@ -1,3 +1,16 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 02
+ * Title:			Pocket Calculator
+ * Author:			Emina Sljivic
+ * ----------------------------------------------------------
+ * Description:
+ * First the user can choose between some operations then
+ * that operation will be performed and the result will
+ * be printed. This is reapiting until the user enter -1.
+ * ----------------------------------------------------------
+ */
 #include <stdio.h>
 #include <float.h>
 
@@ -7,12 +20,12 @@ int print_menu_and_get_operation()
 
   do {
 
-    printf("Choose one of the following operations: \n   Add (1) \n   Subtract (2) \n   Multiply (3) \n   Divide (4) \n   Stop program (-1) \nEnter your choice: ");
+    printf("Choose one of the following operations: \n   Add (1) \n   Subtract (2) \n   Multiply (3) \n   Divide (4) \n   Power (5)\n   Arithmetic_Series (7)\n   Stop program (-1) \nEnter your choice: ");
     scanf("%d",&operation );
-    if(operation != 1 && operation != 2 && operation != 3 && operation !=4 && operation !=-1){
+    if(operation != 1 && operation != 2 && operation != 3 && operation !=4 && operation !=5 && operation !=7 && operation !=-1){
       printf("Input not allowed, please try again!\n");
     }
-  } while(operation != 1 && operation != 2 && operation != 3 && operation !=4 && operation !=-1);
+  } while(operation != 1 && operation != 2 && operation != 3 && operation !=4 && operation !=5 && operation !=7 && operation !=-1);
 
   return operation;
 }
@@ -66,6 +79,15 @@ double perform_operation(int operation,double num1, double num2)
     result=num1*num2;
   }
 
+  else if(operation==5)
+  {
+      result=num1;
+      for (int i = 1; i < num2; i++)
+      {
+          result=result*num1;
+      }
+  }
+
   else if(num2!=0)
   {
       result=num1/num2;
@@ -74,6 +96,35 @@ double perform_operation(int operation,double num1, double num2)
   else
   {
     printf("Division by zero!\n");
+  }
+
+  return result;
+}
+
+double read_one_operand()
+{
+  double operand;
+  do {
+    printf("Please enter the operand: ");
+    scanf("%lf",&operand);
+    if(operand > DBL_MAX)
+    {
+      printf("Number overflow!\n");
+    }
+    else if(operand < 1)
+    {
+      printf("Number too small!\n");
+    }
+  } while(operand > DBL_MAX || operand < 1);
+
+  return operand;
+}
+
+double Arithmetic_Series(double operand)
+{
+  int result=0;
+  for (int i = 1; i <= operand; i++) {
+    result =result + i;
   }
 
   return result;
@@ -89,7 +140,16 @@ int main(int argc, char const *argv[])
 
       operation=print_menu_and_get_operation();
 
-      if(operation!=-1)
+      if(operation==7)
+      {
+        opernad_one=read_one_operand();
+
+        result=Arithmetic_Series(opernad_one);
+
+        printf("The result is: %lf\n",result);
+      }
+
+      else if(operation!=-1)
       {
         get_opernads(&opernad_one, &opernad_two);
         result=perform_operation(operation,opernad_one,opernad_two);
